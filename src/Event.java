640d39989bc65ec4f3195aa7388a308b28eda168
pieworkplace <pieworkplace@gmail.com>
2018-02-13 17:50:11 -0500

    Changed some comment in Teller.

--- src/Event.java
+++ src/Event.java
@@ -2,5 +2,7 @@ public class Event{
     /** This is the super class of both Customer and Teller.
      * Customer represents customer arrival event.
      * Teller represents teller free event.
+     * Time stamp of customer is arrivalTime
+     * Time stamp of teller is freeTime
      */
 }
