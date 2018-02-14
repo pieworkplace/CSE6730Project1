006cb9bc62c2f3e4fa8cdafae3ff283e2b3f0a98
pieworkplace <pieworkplace@gmail.com>
2018-02-13 21:16:18 -0500

    Changed some parameters in parameters

--- src/Parameters.java
+++ src/Parameters.java
@@ -4,15 +4,15 @@ public class Parameters {
      * Change this part before each run.
      */
 
-    public static final int WINDOW_NUM = 4;
+    public static final int WINDOW_NUM = 3;
     public static final int RUNTIME = 3600 * 7;
     public static final double VIP_RATE = 0.2;
 
-    public static final int PATIENCE_AVERAGE = 3600;
-    public static final int PATIENCE_RANGE = 3600;
+    public static final int PATIENCE_AVERAGE = 60 * 30;
+    public static final int PATIENCE_RANGE = 60 * 30;
 
-    public static final int SERVICE_AVERAGE = 900;
-    public static final int SERVICE_RANGE = 900;
+    public static final int SERVICE_AVERAGE = 60 * 15;
+    public static final int SERVICE_RANGE = 60 * 15;
 
     // lambda for poisson distribution of customer arrival
     public static final double LAMBDA = 60 * 5;
